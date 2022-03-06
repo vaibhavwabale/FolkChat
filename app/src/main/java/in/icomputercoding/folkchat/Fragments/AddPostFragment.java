42be99b9b454580c4e2327e5cd26e247e7a75e79
shravanlagad <89975212+shravanlagad@users.noreply.github.com>
2022-03-06 12:48:56 +0530

    AddPostFragment.java

--- app/src/main/java/in/icomputercoding/folkchat/Fragments/AddPostFragment.java
+++ app/src/main/java/in/icomputercoding/folkchat/Fragments/AddPostFragment.java
@@ -1,6 +1,7 @@
 package in.icomputercoding.folkchat.Fragments;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
@@ -9,15 +10,26 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import in.icomputercoding.folkchat.Model.User;
+import in.icomputercoding.folkchat.R;
 import in.icomputercoding.folkchat.databinding.FragmentAddPostBinding;
 
 public class AddPostFragment extends Fragment {
 
     FragmentAddPostBinding binding;
-
+    Uri uri;
+    FirebaseAuth auth;
+    FirebaseDatabase database;
 
     public AddPostFragment() {
 
@@ -27,6 +39,8 @@ public class AddPostFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        auth = FirebaseAuth.getInstance();
+        database = FirebaseDatabase.getInstance();
     }
 
     @Override
@@ -35,6 +49,27 @@ public class AddPostFragment extends Fragment {
 
         binding = FragmentAddPostBinding.inflate(inflater,container,false);
 
+        database.getReference().child("Users")
+                .child(FirebaseAuth.getInstance().getUid()).addListenerForSingleValueEvent(new ValueEventListener() {
+
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+               if (snapshot.exists()) {
+                   User user = snapshot.getValue(User.class);
+                   Picasso.get()
+                           .load(user.getProfile())
+                           .placeholder(R.drawable.placeholder)
+                           .into(binding.profileImage);
+                   binding.name.setText(user.getName());
+                   binding.profession.setText(user.getProfession());
+               }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
         binding.postDescription.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
@@ -76,5 +111,20 @@ public class AddPostFragment extends Fragment {
     }
 
         return binding.getRoot();
+    }
+}
+
+@Override
+public void onActivityResult(int requestCode, int resultCode, @Nullable Intent date) {
+    super.onActivityResult(requestCode, resultCode, data);
+    if (data.getData() != null) {
+        uri = data.getData();
+        binding.postImage.setImageURI(uri);
+        binding.postImage.setVisibility(View.VISIBLE);
+
+        binding.postBtn.setBackgroundDrawable(ContextCompat.getDrawable(getContext(), R.drawable.follow_btn_bg));
+        binding.postBtn.setTextColor(getContext().getResources().getColor(R.color.white));
+        binding.postBtn.setEnabled(true);
+        }
     }
 }
