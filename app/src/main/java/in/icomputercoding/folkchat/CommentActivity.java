eb89c65dfe2422ea9be9e1c7197743ab8ddf16be
Vaibhav Wable <vaibhavsonu0506@gmail.com>
2022-03-09 19:01:13 +0530

    Comment Design Done

--- app/src/main/java/in/icomputercoding/folkchat/CommentActivity.java
+++ app/src/main/java/in/icomputercoding/folkchat/CommentActivity.java
@@ -1,14 +1,28 @@
 package in.icomputercoding.folkchat;
 
-import androidx.appcompat.app.AppCompatActivity;
+import static android.content.ContentValues.TAG;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import in.icomputercoding.folkchat.databinding.ActivityCommentBinding;
 
 public class CommentActivity extends AppCompatActivity {
+
+    ActivityCommentBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_comment);
+        binding = ActivityCommentBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.backArrow.setOnClickListener(v -> {
+            Log.d(TAG, "onClick: navigating back to 'ProfileActivity'");
+            finish();
+        });
+
     }
 }
